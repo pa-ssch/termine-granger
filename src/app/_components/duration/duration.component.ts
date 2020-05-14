dfccfbaa5eccb756bec9181d92445b2654ef26dc
SilasDerProfi <git@schnurr.one>
2020-05-14 22:07:48 +0200

    Duration auf get / set umgabaut

--- src/app/_components/duration/duration.component.ts
+++ src/app/_components/duration/duration.component.ts
@@ -6,35 +6,33 @@ import { durationUnit, getAllUnits } from "src/app/data/types/durationUnit";
   templateUrl: "./duration.component.html",
   styleUrls: ["./duration.component.scss"],
 })
-export class DurationComponent implements OnChanges {
+export class DurationComponent {
   readonly units: durationUnit[] = getAllUnits().sort((a, b) => a.minutes - b.minutes);
   unit: durationUnit = this.units[0];
   duration: number = 0;
 
   @Input()
-  ngModel: number;
+  set ngModel(value: number) {
+    // Größtmögliche Einheit für die Dauer festelgen und den
+    // numerischen "Dauer"-Wert entsprechend anpassen
+    // Voraussetzung hierfür ist, dass die Einheiten
+    // nach Dauer aufsteigend sortiert sind.
+    if (value > 0 && this.duration == 0) {
+      for (let u of this.units) {
+        console.log(u.label);
+        console.log(value % u.minutes);
+        if (value % u.minutes > 0) break;
+
+        this.unit = u;
+        this.duration = Math.floor(value / u.minutes);
+      }
+    }
+  }
 
   @Output()
   ngModelChange = new EventEmitter<number>();
 
   constructor() {}
-
-  ngOnChanges(changes: SimpleChanges): void {
-    // Wenn bereits ein Wert zugewießen war, wird
-    // die Einheit nicht automatisch geändert
-    if (changes?.ngModel?.previousValue || !changes?.ngModel?.currentValue) return;
-
-    // Größtmögliche Einheit für die Dauer festelgen und den
-    // numerischen "Dauer"-Wert entsprechend anpassen
-    // Voraussetzung hierfür ist, dass die Einheiten
-    // nach Dauer aufsteigend sortiert sind.
-    for (let u of this.units) {
-      if (this.ngModel % u.minutes > 0) break;
-
-      this.unit = u;
-      this.duration = this.ngModel / u.minutes;
-    }
-  }
 
   change() {
     if (!this.duration || this.duration < 0) this.duration = 0;

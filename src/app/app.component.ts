76ce1092b4881ca8383a438cc266e334f12a8a57
SilasDerProfi <git@schnurr.one>
2020-06-03 15:03:17 +0200

    * Offline Push benachrichtigungen aktiviert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 
-import { Platform } from "@ionic/angular";
+import { Platform, ToastController } from "@ionic/angular";
 import { SplashScreen } from "@ionic-native/splash-screen/ngx";
 import { StatusBar } from "@ionic-native/status-bar/ngx";
 import { DataService } from "./data/data.service";
@@ -11,7 +11,12 @@ import { DataService } from "./data/data.service";
   styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
-  constructor(private platform: Platform, private splashScreen: SplashScreen, private statusBar: StatusBar) {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    public toastController: ToastController
+  ) {
     this.initializeApp();
   }
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
+import { ToastController, AlertController } from "@ionic/angular";
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-home",
@@ -7,9 +7,48 @@ import { ActivatedRoute } from "@angular/router";
   styleUrls: ["home.page.scss"],
 })
 export class HomePage implements OnInit {
-  constructor() {
+  constructor(private toastController: ToastController, private alertController: AlertController) {
     // wenn erster start --> set root tutorial
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.handleButtonClick();
+  }
+
+  private async handleButtonClick() {
+    if (!("showTrigger" in Notification.prototype)) {
+      const toast = await this.toastController.create({
+        color: "danger",
+        // duration: 5000, -> Dauerhaft anzeigen
+        header: "Erinnerungen nicht verfügbar",
+        message:
+          "Der Browser unterstützt keine Push-Benachrichtigungen, oder die Funktion ist nicht aktiviert.",
+        buttons: [
+          {
+            icon: "help",
+            handler: () => {
+              this.showHelp();
+            },
+          },
+          {
+            icon: "close",
+            role: "cancel",
+          },
+        ],
+      });
+      await toast.present();
+    }
+  }
+
+  private async showHelp() {
+    const alert = await this.alertController.create({
+      header: "Push-Benachrichtigungen nicht verfügbar.",
+      message:
+        "Es wird der Browser Google Chrome benötigt. " +
+        "Ab Version 80 muss das Flag '#enable-experimental-web-platform-features' in 'chrome://flags' aktiviert werden.",
+      buttons: ["OK"],
+    });
+
+    await alert.present();
+  }
 }

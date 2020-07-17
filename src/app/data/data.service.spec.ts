b7998a5c5d20a6360e7665f418e3e5d1c87c631c
SilasDerProfi <silas@schnurr.one>
2020-07-17 12:36:44 +0200

    * teststruktur für Tasklist.addOrChange Test erstellt * automatisch generierte Tests entfernt

--- src/app/data/data.service.spec.ts
+++ src/app/data/data.service.spec.ts
@@ -3,75 +3,106 @@ import { DataService } from "./data.service";
 import { Reminder } from "./reminder";
 import { Task } from "./task";
 
-describe("dataService Testsuite", () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [DataService],
+describe("Testsuite: taskList.addOrChange", () => {
+  it("Spec: Insert Task with wrong Parent", () => {
+    // ...
+  });
+
+  it("Spec: IsDone Changed", () => {
+    // ...
+  });
+
+  describe("Database index ascending order: IX_TASK_START_DATE", () => {
+    it("Spec: Insert Task", () => {
+      // ...
+    });
+
+    it("Spec: Update Task", () => {
+      // ...
+      // test sort value changed (je index)
+      // test changed but not sortcriteria (je index)
+    });
+  });
+  describe("Database index descending order: IX_TASK_START_DATE", () => {
+    it("Spec: Insert Task", () => {
+      // ...
+    });
+
+    it("Spec: Update Task", () => {
+      // ...
+      // test sort value changed (je index)
+      // test changed but not sortcriteria (je index)
     });
   });
 
-  it("Spec: create Datbase", inject([DataService], (service: DataService) => {
-    expect(service).toBeTruthy();
-
-    service.getChildrenCount(0).then((c) => expect(c).toEqual(0));
-  }));
-
-  it("Spec: create Task with Reminders", inject([DataService], (service: DataService) => {
-    let task = Object.assign(new Task(), {
-      title: "Test Task 1",
-      description: "Lorem Ipsum dolor sit amet",
-      duration: 0,
-      priority: 2,
-      isBlocker: true,
-      isDoneDate: "",
-      parentId: 0,
+  describe("Database index ascending order: IX_TASK_DEADLINE", () => {
+    it("Spec: Insert Task", () => {
+      // ...
     });
 
-    let starttime = new Date(task.startTime).getTime();
-    // 2 Erinnerungen: 5 und 15 Minuten vor Start
-    let remlist = [
-      Object.assign(new Reminder(), { reminderTime: new Date(starttime - 60000 * 5).toISOString() }),
-      Object.assign(new Reminder(), { reminderTime: new Date(starttime - 60000 * 15).toISOString() }),
-    ];
-    service.updateTask(task, remlist).then(() => {
-      task.taskId = 1;
-      service.getTasks(0).then((c) => expect(c?.length).toBe(0));
-      service.getTask(1).then((t) => expect(t).toBe(task));
+    it("Spec: Update Task", () => {
+      // ...
+      // test sort value changed (je index)
+      // test changed but not sortcriteria (je index)
     });
-  }));
+  });
+  describe("Database index descending order: IX_TASK_DEADLINE", () => {
+    it("Spec: Insert Task", () => {
+      // ...
+    });
 
-  it("Spec: update Task", inject([DataService], (service: DataService) => {
-    expect(service).toBeTruthy();
-    service.getTask(1).then((task) => {});
-  }));
+    it("Spec: Update Task", () => {
+      // ...
+      // test sort value changed (je index)
+      // test changed but not sortcriteria (je index)
+    });
+  });
 
-  it("Spec: update Reminder", inject([DataService], (service: DataService) => {
-    //expect(service).toBeTruthy();
-  }));
+  describe("Database index ascending order: IX_TASK_TITLE", () => {
+    it("Spec: Insert Task", () => {
+      // ...
+    });
 
-  it("Spec: delete Task", inject([DataService], (service: DataService) => {
-    //expect(service).toBeTruthy();
-  }));
+    it("Spec: Update Task", () => {
+      // ...
+      // test sort value changed (je index)
+      // test changed but not sortcriteria (je index)
+    });
+  });
+  describe("Database index descending order: IX_TASK_TITLE", () => {
+    it("Spec: Insert Task", () => {
+      // ...
+    });
 
-  it("Spec: delete Reminder", inject([DataService], (service: DataService) => {
-    //expect(service).toBeTruthy();
-  }));
+    it("Spec: Update Task", () => {
+      // ...
+      // test sort value changed (je index)
+      // test changed but not sortcriteria (je index)
+    });
+  });
 
-  it("Spec: load Tasklist by startdate", inject([DataService], (service: DataService) => {
-    //expect(service).toBeTruthy();
-  }));
+  describe("Database index ascending order: IX_TASK_PRIORITY", () => {
+    it("Spec: Insert Task", () => {
+      // ...
+    });
 
-  it("Spec: load Tasklist by deadline", inject([DataService], (service: DataService) => {
-    //expect(service).toBeTruthy();
-  }));
+    it("Spec: Update Task", () => {
+      // ...
+      // test sort value changed (je index)
+      // test changed but not sortcriteria (je index)
+    });
+  });
+  describe("Database index descending order: IX_TASK_PRIORITY", () => {
+    it("Spec: Insert Task", () => {
+      // ...
+    });
 
-  it("Spec: load Tasklist by title", inject([DataService], (service: DataService) => {
-    //expect(service).toBeTruthy();
-  }));
-
-  it("Spec: load Tasklist by priority", inject([DataService], (service: DataService) => {
-    //expect(service).toBeTruthy();
-  }));
+    it("Spec: Update Task", () => {
+      // ...
+      // test sort value changed (je index)
+      // test changed but not sortcriteria (je index)
+    });
+  });
 });
 
 // let rem1 = Object.assign(new Reminder(), { reminderId: 0, taskId: 0, reminderTime: "" });
